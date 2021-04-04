926dd03fc12036cbe3ace5182c4f9dec69a8bb9c
beeman <borggreve@gmail.com>
2021-04-04 12:26:24 -0500

    fix: use correct services url

--- libs/services/src/lib/kin-wallet-services.ts
+++ libs/services/src/lib/kin-wallet-services.ts
@@ -25,7 +25,7 @@ export interface BalanceResult {
 }
 
 export class KinWalletService {
-  readonly baseUrl = 'https://services-test.kintegrate.dev/api/'
+  readonly baseUrl = 'https://services.kintegrate.dev/api/'
 
   constructor(private readonly network: Network) {
     console.log(`KinWalletService: environment: ${network.name}`)
